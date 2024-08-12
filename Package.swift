3fe293d475ec5388686c97e13248dcea0ed8bbd7
Alf Watt <alf@istumbler.net>
2024-08-12 16:32:32 -0700

    Remove Swift specific targets
    Always include the KitBridgeAliases.swift

--- Package.swift
+++ Package.swift
@@ -6,23 +6,19 @@ let package = Package(
 	name: "KitBridge",
     platforms: [
         .macOS(.v14),
-        .iOS(.v14),
-        .tvOS(.v14)
+        .iOS(.v15),
+        .tvOS(.v15)
     ],
 	products: [
 		.library(
 			name: "KitBridge",
-			targets: ["KitBridge", "KitBridgeSwift"]
+			targets: ["KitBridge"]
 		)
 	],
 	targets: [
 		.target(
 			name: "KitBridge",
 			path: "Sources/KitBridge"
-		),
-		.target(
-			name: "KitBridgeSwift",
-			path: "Sources/Swift"
-		)		
+		)
 	]
 )
